4bb1ddf1129f33a82e49c43c6d9066c77ac5fcf1
Mr-Strange-Pwn <pawan.tiwari9411@gmail.com>
2020-04-12 13:49:19 +0530

    info added in join component

--- src/components/join/join.jsx
+++ src/components/join/join.jsx
@@ -8,16 +8,24 @@ const Join = () => {
     const [room, setRoom] = useState(''); 
 
     return ( 
+    <React.Fragment>
+       
         <div className="joinOuterContainer">
-            <div className="joinInnerContainer">
+           
+           <div className="joinInnerContainer">
                 <h1 className="heading">join</h1>
                 <div><input placeholder="Name" className="joinInput" type="text" onChange={(e)=>setName(e.target.value)} /></div>
                 <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(e)=>setRoom(e.target.value)} /></div>
                 <Link onClick={e=> (!name || !room)? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
                     <button className="button mt-20">Sign-in</button>
                 </Link>
+                <div className="infoContainer">
+                <p> - its just a simple realtime chat app <br/>click hear to know about me ..
+                <button className="button mt-20" onClick={()=>window.open("https://pawan-tiwari.imfast.io")}><span role="img" aria-label="emoji">👷</span></button> </p>
+            </div>
             </div>
         </div>
+    </React.Fragment>
      );
 }
  
